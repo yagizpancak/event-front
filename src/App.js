59bc77ea8e8b6baed9de2e1b7d4e78eba72e031e
yagiz <yagizpancak@gmail.com>
2023-06-10 22:46:36 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Header from "./components/Layout/Header";
+import { useState } from "react";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setCarIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCarIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCarIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
